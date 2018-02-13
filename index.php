9374e1b35bce827c3ac0c01b382abf51004f0e82
Christian Paul Decembrana <christiandecembrana1@gmail.com>
2018-02-13 15:52:20 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>My First Website </title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  </head>
+  <body>
+
+  <!-- This is For Header -->
+  <div class="col-md-12" id = "header">
+        <h1>HEADER</h1>
+  </div>
+    <!-- end of header -->
+
+
+
+
+
+<!-- This is for body -->
+  <div class="row ">
+
+  <!-- sidebar left -->
+      <div class="col-md-2" id ="sbl">
+          <h5>SIDE BAR LEFT</h5>
+
+      <ul class="list-group">
+            <li class="list-group-item">New <span class="badge">12</span></li>
+            <li class="list-group-item">Deleted <span class="badge">5</span></li>
+            <li class="list-group-item">Warnings <span class="badge">3</span></li>
+      </ul>
+      </div>
+
+  <!-- end of sidebar left -->
+
+
+  <!-- main content -->
+    <div class="col-md-8 text-justify" id = "mc">
+
+      <div id = "mc-bg" >
+
+        <h3 style="transform: translate(35%,0)">Elrick Dave M. Tubig</h3>
+
+      <p style="transform: translate(30%,0)"><strong>Address:</strong> 96 sylvia st. Nepo Subdivision Angeles City </p>
+      <p style="transform: translate(30%,0)"><strong>Hobbies:</strong> VolleyBall, BasketBall</p>
+      <p style="transform: translate(30%,0)"><strong>Skills:</strong> Web Developer, Digital Marketing</p>
+
+
+      </div>
+
+
+    </div>
+
+
+  <!-- end of main content -->
+
+
+
+  <!-- end of space -->
+
+  <!-- sidebar right -->
+    <div class="col-md-2" id = "sbr">
+          <h5>SIDE BAR RIGHT</h5>
+    </div>
+
+  <!-- end of sidebar right -->
+
+
+  </div>
+
+  <!-- end of body -->
+
+
+  <!-- footer -->
+<div class="col-md-12" id = "footer" >
+    <h4>Alright reserved &copy EDMT</h4>
+
+</div>
+
+  <!-- end of footer -->
+
+
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js"> </script>
+
+  </body>
+
+</html>
